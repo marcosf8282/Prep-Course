91d82f60f07daaf55957a5a3d8c32e1b10bc9406
unknown <marcosf3493@gmail.com>
2022-09-08 21:29:15 -0300

    tarea extra clase

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,9 +10,14 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
-  console.log(Object.values(objeto))
+  var matriz = [];
+
+  for(var propiedad in objeto){
+    matriz.push([propiedad,objeto[propiedad]])
+  }
+
+  return matriz
 }
-deObjetoAmatriz({D: 1, B: 2, C: 3})
 
 
 function numberOfCharacters(string) {
@@ -20,6 +25,19 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var cantCaracteres = {};
+
+  for (var i = 0; i < string.length; i++) {
+
+    if(cantCaracteres.hasOwnProperty(string[i])){
+      cantCaracteres[string[i]] ++;
+    }else{
+      cantCaracteres[string[i]] = 1;
+    }
+    
+  }
+
+  return cantCaracteres
 }
 
 
@@ -28,11 +46,19 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
-  const rev = s.Reverse();
-  console.log(rev)
-}
-capToFront('Marcos')
+  var mayus = '';
+  var minus = '';
 
+  for (var i = 0; i < s.length; i++) {
+    if(s[i] !== s[i].toUpperCase()){
+      minus += s[i]
+    }else{
+      mayus += s[i]
+    }
+  }
+
+  return mayus + minus
+}
 
 function asAmirror(str) {
   //La función recibe una frase. 
@@ -40,6 +66,15 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  var palabrasSeparadas = str.split(' ');
+
+  var fraseInvertida = [];
+
+  for (var i = 0; i < palabrasSeparadas.length; i++) {
+    fraseInvertida.push(palabrasSeparadas[i].split(' ').reverse().join(' '));
+  }
+
+  return fraseInvertida.join(' ');
 } 
 
 
@@ -48,6 +83,16 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var num = numero.toString();
+
+  for (let i = 0; i < num.length/2; i++) {
+
+    if((num[i] !== num[num.length - 1 - i])){
+      return 'No es capicua'
+    }
+    return 'Es capicua'
+
+  }
 }
 
 
@@ -55,6 +100,17 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var palabra = '';
+
+  for (var i = 0; i < cadena.length; i++) {
+
+    if(cadena[i] !== 'a' && cadena[i] !== 'b' && cadena[i] !== 'c'){
+      palabra += cadena[i]
+    }
+
+  }
+
+  return palabra
 }
 
 
@@ -62,6 +118,23 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  do{
+    var cambios = false;
+
+    for (var i = 0; i < arr.length - 1; i++) {
+
+      if (arr[i].length > arr[i + 1].length) {
+        var store = arr[i + 1];
+        arr[i + 1] = arr[i];
+        arr[i] = store;
+        cambios = true;
+      }
+
+    }
+
+  }while(cambios);
+
+  return arr;
 }
 
 
@@ -71,6 +144,22 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+  var arregloVacio = [];
+
+  for (let i = 0; i < arreglo1.length; i++) {
+    var element = arreglo1[i];
+
+    for (let j = 0; j < arreglo2.length; j++) {
+
+      if (element === arreglo2[j]) {
+        arregloVacio.push(element);
+      }
+
+    }
+
+  }
+
+  return arregloVacio;
 }
 
 
